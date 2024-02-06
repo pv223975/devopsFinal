6dc6cad75abd1f5463000b810d44a5504be205ac
Luke <tarquin2001@gmail.com>
2024-02-06 19:48:19 +0000

    Created user create page and added create button to default page.

--- project/app.py
+++ project/app.py
@@ -87,6 +87,21 @@ def login():
             return redirect(url_for("index"))
     return render_template("login.html", error=error)
 
+@app.route("/create_user", methods=["GET", "POST"])
+def create_user():
+    """User login/authentication/session management."""
+    error = None
+    if request.method == "POST":
+        if request.form["username"] != app.config["USERNAME"]:
+            error = "Invalid username"
+        elif request.form["password"] != app.config["PASSWORD"]:
+            error = "Invalid password"
+        else:
+            session["logged_in"] = True
+            flash("You were logged in")
+            return redirect(url_for("index"))
+    return render_template("cuser.html", error=error)
+
 
 @app.route("/logout")
 def logout():
