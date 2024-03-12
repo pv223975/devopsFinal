9bedc9b63aff6c5d325c5de6624bddca854bc491
pheng@umontana.edu <pheng@umontana.edu>
2024-03-12 19:29:11 +0000

    cleaned up app_test

--- tests/app_test.py
+++ tests/app_test.py
@@ -19,6 +19,7 @@ def client():
     with app.app_context():
         db.create_all()  # setup
 
+        # creates user for tests to reference
         new_entry = User(uName='testuser', pWord='password')
         db.session.add(new_entry)
         db.session.commit()
@@ -26,7 +27,6 @@ def client():
         yield app.test_client()  # tests run here
         db.session.remove()
         db.drop_all()  # teardown
-
 
 def login(client, username, password):
     """Login helper function"""
@@ -36,11 +36,9 @@ def login(client, username, password):
         follow_redirects=True,
     )
 
-
 def logout(client):
     """Logout helper function"""
     return client.get("/logout", follow_redirects=True)
-
 
 def test_index(client):
     response = client.get("/", content_type="html/text")
@@ -51,18 +49,19 @@ def test_database(client):
     tester = Path("test.db").is_file()
     assert tester
 
-# altered to read db instead of alert
-# TESTING to see if writing to test.db
-# def test_empty_db(client):
-#     """Ensure database is blank"""
-#     table_status = db.session.query(User).first()
-#     assert table_status is not None
-
-# TESTING if writing to test.db
-def test_written_db(client):
-    """Ensure database is NOT blank"""
+def test_db_status(client):
+    # # ALTERED test_empty_db TO READ DB INSTEAD OF RELYING ON ALERTS
+    # """Ensure database is blank"""
+    # table_status = db.session.query(User).first()
+    # assert table_status is not None
+    """Test if DB was written on"""
     table_status = db.session.query(User).first()
     assert table_status is not None
+
+def test_added_user(client):
+    """Test if testuser was input to DB"""
+    retrieved_user = db.session.query(User).filter_by(uName="testuser").first()
+    assert retrieved_user is not None
 
 def test_login_logout(client):
     """Test login and logout using helper functions"""
@@ -71,35 +70,21 @@ def test_login_logout(client):
     rv = logout(client)
     assert b"You were logged out" in rv.data
 
-# TEST to see if added user exist
-def test_added_user(client):
-    # Create a user and add it to the database
-    # new_entry = User(uName="testuser", pWord="password")
-    # db.session.add(new_entry)
-    # db.session.commit()
-
-    # Verify that the user exists in the database
-    retrieved_user = db.session.query(User).filter_by(uName="testuser").first()
-    assert retrieved_user is not None
-
-# test2 - Login SUCCEEDS testuser:password
-def test_testuser(client):
-    """Test login and logout using helper functions"""
-
+def test_testuser_pass(client):
+    """Test login succeeds with valid user and password"""
     rv = login(client, 'testuser', 'password')
     assert b"Logged in:" in rv.data
     rv = logout(client)
 
-# test3 - should fail login testuser:wrongpassword
-def test_wrongpass(client):
-    """Test login with incorrect password"""
+def test_testuser_wrongpass(client):
+    """Test login fail with incorrect password"""
     rv = login(client, "testuser" , "wrongpassword")
     assert b"Invalid username or password" in rv.data
     rv = logout(client)
 
 # test1 - error when navigating while loggedout
 def test_access_without_login(client):
-    """Try to access without login"""
+    """Test error when navigating without login"""
     rv = logout(client)
     response = client.post("/add")
     assert response.status_code == 401
@@ -113,8 +98,9 @@ def test_messages(client):
         follow_redirects=True,
     )
     assert b"New entry was successfully posted" in rv.data
+    logout(client)
 
-
+# DISABLED BY LUKE - NEED TO FIX
 # def test_delete_message(client):
 #     """Ensure the messages are being deleted"""
 #     rv = client.get("/delete/1")
